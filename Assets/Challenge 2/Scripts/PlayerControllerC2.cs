a21a597863452e31eae85303918708110b9d454d
david-sanchez17 <david.sanchez17@g.austincc.edu>
2026-06-21 22:09:34 -0500

    Adjustments to spawn manager and player controller

--- Assets/Challenge 2/Scripts/PlayerControllerC2.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerC2.cs
@@ -4,16 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerControllerC2 : MonoBehaviour
 {
     public GameObject dogPrefab;
-    public float fireDelay = 20.0f;  
+   
 
     // Start is called before the first frame update
     void Update()
     {
-        fireDelay -= 0.1f;
-        if (Input.GetKeyDown(KeyCode.Space) && fireDelay <= 0.0f)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-            fireDelay = 20.0f;
+         
         }
     }
 }
